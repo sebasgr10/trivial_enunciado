87c5af2a043bebc6d08fd56fa71e8edf95370591
Steven Sebastian Guerrero Rumipamba <stevens.guerrerorumipamba@iesfm.onmicrosoft.com>
2022-03-01 12:39:25 +0100

    eliminamos código repetido de posición

---
+++ test/TrivialTests.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import smellytrivial.Game;
+
+public class TrivialTests {
+    @Test
+    public void true_is_true(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void crear_Game(){
+        Game trivial = new Game();
+    }
+
+    @Test
+    public void si_al_principio_saco_un_1_voy_a_casila_1()
+    {
+        //Arrange
+        Game sut = new Game();
+        sut.agregar("María");
+        sut.agregar("Juan");
+
+        sut.tirarDado(1);
+
+        String expected = "La nueva posición de María es 1";
+
+        //Act
+        String actual = sut.nuevaPosicionJugador();
+
+        //Assert
+        Assertions.assertEquals(expected, actual);
+    }
+}
